55a822b8f97ba12afffe65f390cdcdaa417205f9
Ed-D1 <eddie943@outlook.com>
2019-11-28 18:26:31 -0600

    an example of initializing a int array with specified values

--- example/src/example/Examples.java
+++ example/src/example/Examples.java
@@ -4,8 +4,11 @@ public class Examples {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("hello world");
-		System.out.println("second Commit");
+		System.out.printf("%s%8s%n,", "index", "Value");
+		int []array = {10, 29, 48, 70, 39};
+		for(int counter = 0; counter < array.length; counter++) {
+			System.out.printf("%5d%8d%n", counter, array[counter]);
+		}
 
 	}
 
